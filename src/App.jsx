2bf94d8baa5f1314c42f406ed96e68ab7079e329
Daren Morreale <daren@givelegacy.com>
2025-02-18 21:55:42 -0500

    updated app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import "./App.css";
+import { useState } from "react";
 import { puppyList } from "./data.js";
 
 function App() {
-  console.log(puppyList);
+  const [puppies, setPuppies] = useState(puppyList);
+  console.log(puppies);
+
   return (
     <>
-      <div>{puppyList}</div>
+      <div className="app">
+        {puppies.map((puppy) => {
+          return <p key={puppy.id}>{puppy.name}</p>;
+        })}
+      </div>
     </>
   );
 }
