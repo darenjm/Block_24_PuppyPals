476ea07e0170be896f3e7f31b6cd98f8f949a63a
Daren Morreale <daren@givelegacy.com>
2025-02-20 17:39:56 -0500

    Added puppu id

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,18 @@ import { puppyList } from "./data.js";
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
   console.log(puppies);
+
+  function handleClick() {
+    //
+  }
 
   return (
     <>
       <div className="app">
         {puppies.map((puppy) => {
-          return <p key={puppy.id}>{puppy.name}</p>;
+          return <p onClick={()=>{console.log("puppy id: ", puppy.id)}} key={puppy.id}>{puppy.name}</p>;
         })}
       </div>
     </>
